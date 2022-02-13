c6a67c07834d0678a63590d248cc81c9a2b5c90a
Dylan Anderson <dylana1998@gmail.com>
2022-02-13 14:29:09 -0600

    Thread safety as well as generic message url and crash notify url

--- main.py
+++ main.py
@@ -16,14 +16,20 @@ flask = Flask(__name__)
 
 client = discord.Client()
 
-toDo = None
+blockz = None
+crashes = None
 
 messageQ = asyncio.Queue()
 
+deploy_semaphore = threading.Semaphore()
+
 
 def redeploy_script():
+    deploy_semaphore.acquire()
+    client.loop.create_task(blockz.send("Push to blockz...web server redeploying..."))
     call("./redeploy.sh")
-    client.loop.create_task(toDo.send("Server Running"))
+    client.loop.create_task(blockz.send("Server Running"))
+    deploy_semaphore.release()
 
 
 def redeploy_thread():
@@ -31,34 +37,51 @@ def redeploy_thread():
     deploy.start()
 
 
-@flask.route("/", methods=["POST", "GET"])
+@flask.route("/redeploy", methods=["POST", "GET"])
 def redeploy():
     try:
         content = request.get_json()
     except Exception:
         print("didnt get any post data")
     redeploy_thread()
-    client.loop.create_task(toDo.send("Push to toDo...web server redeploying..."))
     return "", 200
+
+
+@flask.route("/notifycrash", methods=["POST"])
+def notify():
+    content = request.get_json()
+    client.loop.create_task(crashes.send(content["message"]))
+    return "", 200
+
+
+@flask.route("/message", methods=["POST"])
+def message():
+    content = request.get_json()
+    client.loop.create_task(blockz.send(content["message"]))
+    return "", 200
+
 
 
 @flask.route("/stop")
 def stop():
-    client.loop.create_task((toDo.send("STOP")))
+    client.loop.create_task((blockz.send("STOP")))
     return "", 200
 
 
 @client.event
 async def on_ready():
     print("ready")
-    global toDo
+    global blockz
+    global crashes
     channels = client.get_all_channels()
     for channel in channels:
-        if channel.name == "todo":
-            toDo = channel
+        if channel.name == "blockz":
+            blockz = channel
             fl = threading.Thread(target=start_flask)
             fl.start()
             redeploy_thread()
+        if channel.name == "crashes":
+            crashes = channel
 
 
 async def stop_discord():
@@ -76,8 +99,8 @@ async def send_discord():
         print("Sending Message to Discord")
         mes = await messageQ.get()
         if mes == "STOP":
-            client.loop.stop()
-        await toDo.send("Test Message from a discord bot saying there has been a push to git hub and the web server is being redoployed")
+            sys.exit(0)
+        await blockz.send("Test Message from a discord bot saying there has been a push to git hub and the web server is being redoployed")
 
 
 def start_discord():
